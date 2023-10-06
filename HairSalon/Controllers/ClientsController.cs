e7c5d0ba627a4f8dbced05df94ce2b24d69dcdcf
ian-bravo <bravo.ian@gmail.com>
2023-10-06 14:02:28 -0700

    add form to add client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,9 +16,30 @@ namespace HairSalon.Controllers
       _db = db;
     }
 
+    public ActionResult Index()
+    {
+    List<Client> model = _db.Clients
+                          .Include(client => client.Stylist)
+                          .ToList();
+      return View(model);
+    }
+
     public ActionResult Create()
     {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
